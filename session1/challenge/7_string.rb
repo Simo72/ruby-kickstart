30e7d94f99bc990d744d0e99dfa72ef8da5dcbc1
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-04-20 08:54:14 +0100

    Challenge 1:7

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,25 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+  after_r = []
+  if string.include? ("r") || string.include?("R")
+
+  for r in 0..string.size
+    if string[r] == "r" || string[r] =="R"
+      ans = r + 1
+      after_r << string[ans]
+    end
+  end
+    return after_r.join
+  else
+    ""
+  end
 end
+
+puts pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+puts pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+puts pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
