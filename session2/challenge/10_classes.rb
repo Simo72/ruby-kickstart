f0369e59e0fce66c171376f2901377569924aca5
IAN SIMPSON <ianconradsimpson@gmail.com>
2017-04-22 13:23:12 +0100

    challenge 2:10

--- session2/challenge/10_classes.rb
+++ session2/challenge/10_classes.rb
@@ -16,4 +16,28 @@
 #
 
 class Person
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+    attr_accessor "name", "age"
+
+    def birthday
+      @age = age + 1
+    end
 end
+
+josh = Person.new 'Josh', 28
+
+p josh.name     # => "Josh"
+p josh.age      # => 28
+
+josh.name = 'Joshua'
+p josh.name     # => "Joshua"
+
+josh.birthday # => 29
+p josh.age      # => 29
+
+josh.birthday # => 30
+p josh.age      # => 30
